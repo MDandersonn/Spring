d52fcdda85f61d814b2011e5368da21d077bccca
MDandersonm <1215kimyj@gmail.com>
2023-03-13 20:18:06 +0900

    feat : [김영진] KH스프링 학생,학급 테이블 저장 ,조회 [KHBOARD04]

--- home_demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
+++ home_demo/src/test/java/com/example/demo/forTest/classRoom/ClassRoomTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -18,10 +19,10 @@ public class ClassRoomTest {
     private ClassRoomRepository classRoomRepository;
     @Test
     public void 학생_정보_저장() {
-        ClassRoom classRoom = new ClassRoom("첫번째 클래스");
+        ClassRoom classRoom = new ClassRoom("2번째 클래스");
         classRoomRepository.save(classRoom);
 
-        TestStudent testStudent = new TestStudent("하이");
+        TestStudent testStudent = new TestStudent("하이2-2");
 
         testStudent.setClassRoom(classRoom);
         studentRepository.save(testStudent);
@@ -32,5 +33,17 @@ public class ClassRoomTest {
         TestStudent testStudent = maybeStudent.get();
 
         System.out.println(testStudent);
+    }
+
+    @Test
+    public void 학생_전체_조회() {
+        List<TestStudent> testStudentList = studentRepository.findAll();
+        System.out.println(testStudentList);
+    }
+
+    @Test
+    public void 반이름에_따른_학생_조회() {
+        List<TestStudent> studentList = studentRepository.findStudentListByRoomName("두번째 클래스");
+        System.out.println(studentList);
     }
 }
