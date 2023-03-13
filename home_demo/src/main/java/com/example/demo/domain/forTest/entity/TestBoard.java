deba6aa5a26980e83a87e59560c4222bfaa68421
MDandersonm <1215kimyj@gmail.com>
2023-03-13 17:28:17 +0900

    feat : [김영진] 조인 테이블 구현 [KHBOARD01]

---
+++ home_demo/src/main/java/com/example/demo/domain/forTest/entity/TestBoard.java
@@ -0,0 +1,38 @@
+package com.example.demo.domain.forTest.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class TestBoard {
+    @Id
+    @Column(name = "board_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+//    mappedBy는 참조 엔티티의 속성명을 의미한다.
+//    즉,  comment 엔티티에서 testBoard 엔티티를 참조한 속성명 testBoard을 mappedBy에 전달해야 한다.
+
+    //one: testBoard many: comments
+    @OneToMany(mappedBy = "testBoard", fetch = FetchType.EAGER)
+    private List<Comment> comments = new ArrayList<>();
+
+    public TestBoard(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public void setComment (Comment comment) {
+        comments.add(comment);
+        comment.setTestBoard(this);
+    }
+}
