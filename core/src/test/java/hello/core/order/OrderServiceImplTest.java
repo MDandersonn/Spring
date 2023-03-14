8ce187a7e12c7865ccfedd101da70e4451698829
MDandersonn <kimyj1215@naver.com>
2023-03-14 13:03:05 +0900

    fix: [김영진] 스프링핵심원리-김영한 오류수정 [CORE12]

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,22 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class OrderServiceImplTest {
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+}
