25c6c8f8b4577e9e48e8e7a55fb8fb3dd87bd2ff
MDandersonn <kimyj1215@naver.com>
2023-03-16 01:01:33 +0900

    init: [김영진] 스프링부트 아프리카도서관 쇼핑몰만들기 [INIT]

---
+++ africaBackend/src/main/java/org/africalib/gallery/africabackend/AfricaBackendApplication.java
@@ -0,0 +1,13 @@
+package org.africalib.gallery.africabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfricaBackendApplication.class, args);
+    }
+
+}
